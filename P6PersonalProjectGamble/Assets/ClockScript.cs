48de6e38101c583d0478bc27de0834f4fbf708ef
GavinGamble <710145@student.dusd.net>
2023-12-07 17:08:48 -0800

    12/7/2023

---
+++ P6PersonalProjectGamble/Assets/ClockScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject secondsHandle, minutesHandle, hoursHandle;
+    private float secondsMultiplier = 1f;
+    private int inGameSeconds;
+    private int timeAtStart = 10000;
+
+    void Update()
+    {
+        inGameSeconds = Mathf.RoundToInt(Time.time * secondsMultiplier) + timeAtStart;
+        secondsHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 60, 0);
+        minutesHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 3600, 0);
+        hoursHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 43200, 0);
+    }
+}
